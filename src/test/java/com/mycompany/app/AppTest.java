0d3317d4d6bb4dcf1b891a6c3b7b3536e4cea3ef
Christian DOBE <117773557+ComeDobe@users.noreply.github.com>
2024-06-11 12:54:12 +0200

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,20 +1,32 @@
 package com.mycompany.app;
 
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+    
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testMethod1() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testMethod2() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testMethod3() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testMethod4() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testMethod5() {
+        assertTrue(true);
     }
 }
